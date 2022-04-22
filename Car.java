2540641312c8c251055f13fde8ff495ab62fddda
Maivizhi-C <104206719+Maivizhi-C@users.noreply.github.com>
2022-04-22 23:12:14 +0530

    parking lot basic code

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car extends Vehicle  
+{  
+
+public Car(String licPlate, String companyName)  
+{  
+slotsNeeded = 1;  
+siz = VSize.CarSize;  
+this.licPlate = licPlate;  
+this.companyName = companyName;  
+}  
+  
+public boolean canFitInSlot(ParkingSlot spot)  
+{  
+return spot.getSize() == VSize.CarSize;  
+}  
+}  
